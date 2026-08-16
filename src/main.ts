450674335c2c9a5fa294696fce313e4371497b6a
Benjamin <f.benjamin.larsson@gmail.com>
2026-08-16 22:43:21 +0200

    Debug frametime

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { render as renderScene } from "./rendering/renderer.ts";
 if (DEBUG) {
 	console.log("ℹ️ DEBUG BUILD");
 	var debugDiv = document.body.appendChild(document.createElement("div"));
-	debugDiv.style = "color: yellow;";
+	debugDiv.style = "color: yellow; font-family: monospace";
 }
 
 let previouseFrameTimestamp = 0;
@@ -21,22 +21,17 @@ function onAnimationFrame(timestamp: number) {
 	timerAccumulator += elapsed;
 
 	while (timerAccumulator >= 1000 / 60) {
+		const start = (DEBUG && performance.now()) as number;
 		timerAccumulator -= 1000 / 60;
 
 		renderScene();
-		if (DEBUG) {
-			if (wasKeyJustPressed("Space")) {
-				console.log("Pressed space!");
-			}
-			if (wasKeyJustReleased("Space")) {
-				console.log("Released space!");
-			}
-			const fps = 1000 / elapsed;
-			debugDiv.textContent = `${Math.round(fps)} | Space held: ${isKeyHeld("Space")}`;
-		}
 		consumeInput();
+		if (DEBUG) {
+			const fps = 1000 / elapsed;
+			const frameDuration = performance.now() - start;
+			debugDiv.textContent = `${Math.round(fps)} | ${frameDuration.toFixed(2)}ms | Space held: ${isKeyHeld("Space")}`;
+		}
 	}
-
 }
 
 requestAnimationFrame(onAnimationFrame);
