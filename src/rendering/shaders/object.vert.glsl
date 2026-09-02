f628769f3bc044924240f28c04751b2405964cf3
Benjamin <f.benjamin.larsson@gmail.com>
2026-09-02 23:06:46 +0200

    Minify vert shader a bit

--- src/rendering/shaders/object.vert.glsl
+++ src/rendering/shaders/object.vert.glsl
@@ -7,23 +7,11 @@ out vec4 v; // .xyz = local vertex pos, .w = surface ID
 void main() {
 	vec3 P = p.xyz;
 	P.z *= l;
-	float r = 2.5;
+	// radius = 3.
 	if (b > 0. && P.z > b) {
-		float t = (P.z - b) / r;
-		float angle = t * 1.57;
-		float z0 = b;
-		P.z = z0 + r * sin(angle);
-		P.y -= r * (1.0 - cos(angle));
+		P.y -= 3. * (1.0 - cos((P.z - b) / 3. * 1.57));
+		P.z = b + 3. * sin((P.z - b) / 3. * 1.57);
 	}
-	// if (P.z >= b + radius) {
-		// P.z = b + radius;
-		// P.y = radius * (-P.z + (b - radius));
-	// }
-	// P.y -= .5 * l * sin(1.57 * clamp(((p.z - .5) / .5), 0., 1.));
-	// P.z += cos(1.57 * clamp(((p.z - .5) / .5), 0., 1.));
-	// P.y += sin(p.z * 1.57) * 5.;
-	// P.z -= sin(p.z * 3.14);
-	// P.y += sin(P.z * (l-1.));
 	gl_Position = w * o * vec4(P, 1);
 	v = p;
 }
